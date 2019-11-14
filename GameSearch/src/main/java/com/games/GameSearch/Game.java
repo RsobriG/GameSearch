929fed6f44f92a915dbe5a6e56b960ea20ac13b6
mati <mati@DESKTOP-OLQJ7TQ>
2019-11-14 13:36:59 +0100

    Without Lombok second update

--- GameSearch/src/main/java/com/games/GameSearch/Game.java
+++ GameSearch/src/main/java/com/games/GameSearch/Game.java
@@ -4,7 +4,6 @@ package com.games.GameSearch;
 
 import java.sql.Date;
 import java.util.Calendar;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,18 +14,17 @@ import lombok.Data;
 
 @Entity
 @Table(name="games")
-public @Data class Game{
+public class Game{
+
 
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="gameId", updatable = false, nullable = false)
+	@Column(name="gameId", updatable = true, nullable = false)
 	private long gameId;
 
-	
 	@Column(name="name")
 	private String name;
-
 	
 	@Column(name="description")
 	private String description;
@@ -71,5 +69,104 @@ public @Data class Game{
 		this.name = name;
 		
 	}
+	
+	//Getter and Setters
+	
+	public long getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(long gameId) {
+		this.gameId = gameId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Calendar getTimeGame() {
+		return timeGame;
+	}
+
+	public void setTimeGame(Calendar timeGame) {
+		this.timeGame = timeGame;
+	}
+
+	public int getMaxPlayers() {
+		return maxPlayers;
+	}
+
+	public void setMaxPlayers(int maxPlayers) {
+		this.maxPlayers = maxPlayers;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public GameType getGametype() {
+		return gametype;
+	}
+
+	public void setGametype(GameType gametype) {
+		this.gametype = gametype;
+	}
+
+	public GameState getGamestate() {
+		return gamestate;
+	}
+
+	public void setGamestate(GameState gamestate) {
+		this.gamestate = gamestate;
+	}
+
 
 }
--- GameSearch/src/main/java/com/games/GameSearch/User.java
+++ GameSearch/src/main/java/com/games/GameSearch/User.java
@@ -1,21 +1,17 @@
 package com.games.GameSearch;
 
 
-import java.io.Serializable;
 import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Data;
+//import lombok.Data;
 
 
 @Entity
-
-
-
 @Table (name = "Users")
- public @Data class User {
+ public class User {
 
 	@Id
 	@Column(name = "nickName")
@@ -46,8 +42,88 @@ import lombok.Data;
 	private String birthday;
 	
 	//Constructor
+	
+	public User() {
+		super();
+	}
+	
 	public User(String name) {
 		super();
 		this.name = name;
 	}
+
+	
+	//Getters and Setters
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getHomeLat() {
+		return homeLat;
+	}
+
+	public void setHomeLat(double homeLat) {
+		this.homeLat = homeLat;
+	}
+
+	public double getHomeLong() {
+		return homeLong;
+	}
+
+	public void setHomeLong(double homeLong) {
+		this.homeLong = homeLong;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
 }
