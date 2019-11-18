1a51219aa6d4a0fc6fdf0cf8fd67e578e952c614
Usuario <Usuario@PC-Habitacin>
2019-11-18 19:00:11 +0100

    tablas de asignacion ok

--- GameSearch/src/main/java/com/games/GameSearch/GameSesionController.java
+++ GameSearch/src/main/java/com/games/GameSearch/GameSesionController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.games.GameSearch.GameSesion.GRelation;
+
 
 
 
--- GameSearch/src/main/java/com/games/GameSearch/UserRelationService.java
+++ GameSearch/src/main/java/com/games/GameSearch/UserRelationService.java
@@ -1,5 +1,6 @@
 package com.games.GameSearch;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
@@ -16,10 +17,35 @@ public class UserRelationService {
 	RestTemplate template = new RestTemplate();
 	
 	public Iterable<UserRelation> findbyUserId(String userid){
-		HttpEntity<List<UserRelation>> response = template.exchange("http://localhost:8083/webapi/myrelations",
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<UserRelation>>() {});
-		List<UserRelation> myrelations = response.getBody();
-		return myrelations;
+		HttpEntity<ArrayList<UserRelation>> response = template.exchange("http://localhost:8083/webapi/myrelations/?userid="+userid,
+				HttpMethod.GET, null, new ParameterizedTypeReference<ArrayList<UserRelation>>() {});
+		ArrayList<UserRelation> myrelations = response.getBody();
+		HttpEntity<ArrayList<UserRelation>> response2 = template.exchange("http://localhost:8083/webapi/mytargets/?userid="+userid,
+				HttpMethod.GET, null, new ParameterizedTypeReference<ArrayList<UserRelation>>() {});
+		ArrayList<UserRelation> mytargets = response2.getBody();
+		ArrayList<UserRelation> lastrelations = new ArrayList<UserRelation>();
+		UserRelation temp = new UserRelation();
+		for(UserRelation i:mytargets) {
+			myrelations.add(i);
+		}
+		for(UserRelation i:myrelations) {
+			temp = i;
+			for(UserRelation z:myrelations) {
+				
+				if ((i.getUserId().contentEquals(z.getUserId()) && i.getUserTargetId().equals(z.getUserTargetId())) || (i.getUserId().equals(z.getUserTargetId()) && i.getUserTargetId().equals(z.getUserId()))) {					
+					if(z.gettStamp().after(i.gettStamp())) {
+						temp=z;
+					}					
+				}}
+			if(lastrelations.contains(temp)) {
+					
+			}else {
+				lastrelations.add(temp);
+			}			
+			}
+		
+		
+		return lastrelations;
 	}
 	
 	public void addUserRelation(UserRelation rel) {
